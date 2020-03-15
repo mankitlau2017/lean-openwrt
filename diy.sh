ae7b681e3188f4387727fd042f30439ff7d6d311
mankitlau2017 <30903152+mankitlau2017@users.noreply.github.com>
2020-03-15 12:27:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.112.1/g' package/base-files/files/bin/config_generate
 sed -i 's/Openwrt/mAnkltIau/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/mAnkItlau/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a
