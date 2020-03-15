2e611af88500b3c37ff66463cdd2f356f0723df5
mankitlau2017 <30903152+mankitlau2017@users.noreply.github.com>
2020-03-15 11:54:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Xiaomi/g' package/base-files/files/bin/config_generate
+sed -i 's/Openwrt/mAnkltIau/g' package/base-files/files/bin/config_generate
 ./scripts/feeds update -a
 ./scripts/feeds install -a
